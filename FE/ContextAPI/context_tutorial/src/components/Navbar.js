d045e6f909fd4a51fafa08aa0d802edf7fed5d32
juser0719 <sinsa0719@naver.com>
2021-06-01 00:05:19 +0900

    access&update

--- FE/ContextAPI/context_tutorial/src/components/Navbar.js
+++ FE/ContextAPI/context_tutorial/src/components/Navbar.js
@@ -1,27 +1,32 @@
 import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 function Navbar () {
     // const context = useContext(ThemeContext)
     return (
-      <ThemeContext.Consumer>{(context) =>{
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return(
-          <nav style={{background: theme.ui, color: theme.syntax }}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        )
-      }}
-
-      
-      
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(context) =>{
+          const { isAuthenticated , toggleAuth} = authContext
+          const { isLightTheme, light, dark } = context;
+          const theme = isLightTheme ? light : dark;
+          return(
+            <nav style={{background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <div onClick ={toggleAuth}>
+                {isAuthenticated ? 'Logged in' : 'Logged out '}
+              </div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
+          )
+        }}
+        </ThemeContext.Consumer>
+      )}
+      </AuthContext.Consumer>
     );
 
 }
