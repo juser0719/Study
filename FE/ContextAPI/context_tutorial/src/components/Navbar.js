cb98075bde5e0149f9e49566d930a9b6d2ae82ba
juser0719 <sinsa0719@naver.com>
2021-05-31 17:27:42 +0900

    create context

---
+++ FE/ContextAPI/context_tutorial/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+function Navbar () { 
+    return ( 
+      <nav>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    );
+
+}
+
+ 
+export default Navbar;
