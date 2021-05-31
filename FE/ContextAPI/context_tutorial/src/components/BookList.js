83b486aec6220c97f628513949e809aa94b9a633
juser0719 <sinsa0719@naver.com>
2021-05-31 18:31:52 +0900

    accessing context

--- FE/ContextAPI/context_tutorial/src/components/BookList.js
+++ FE/ContextAPI/context_tutorial/src/components/BookList.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, useContext} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 function BookList () { 
+  const context = useContext(ThemeContext)
+  const {isLightTheme , light, dark} = context
+  const theme = isLightTheme ? light : dark;
+  
     return ( 
-      <div className="book-list">
+      <div className="book-list" style={{color : theme.syntax, background : theme.bg}}>
         <ul>
-          <li>the way of kings</li>
-          <li>the name of the wind</li>
-          <li>the final empire</li>
+          <li style={{background: theme.ui}}>the name of the wind</li>
+          <li style={{background: theme.ui}}>the way of kings</li>
+          <li style={{background: theme.ui}}>the final empire</li>
         </ul>
       </div>
     );
--- FE/ContextAPI/context_tutorial/src/components/Navbar.js
+++ FE/ContextAPI/context_tutorial/src/components/Navbar.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-function Navbar () { 
-    return ( 
-      <nav>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+function Navbar () {
+    // const context = useContext(ThemeContext)
+    return (
+      <ThemeContext.Consumer>{(context) =>{
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return(
+          <nav style={{background: theme.ui, color: theme.syntax }}>
+            <h1>Context App</h1>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        )
+      }}
+
+      
+      
+      </ThemeContext.Consumer>
     );
 
 }
