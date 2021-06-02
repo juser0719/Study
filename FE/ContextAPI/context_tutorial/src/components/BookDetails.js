619c1142f56680147325e8e6e3e3639027ff65a5
juser0719 <sinsa0719@naver.com>
2021-06-02 22:18:45 +0900

    Reducer

--- FE/ContextAPI/context_tutorial/src/components/BookDetails.js
+++ FE/ContextAPI/context_tutorial/src/components/BookDetails.js
@@ -2,10 +2,11 @@ import React , {useContext }from 'react'
 import { BookContext } from '../contexts/BookContext'
 
 function BookDetails ({book}) {
-    const {removeBook} = useContext(BookContext)
+    const {dispatch} = useContext(BookContext)
 
     return(
-    <li onClick ={() => removeBook(book.id)}>
+    <li onClick ={() => dispatch({type: 'REMOVE_BOOK', id: book.id})}>
+     {/* <li onClick ={() => removeBook(book.id)}> */}
         <div className ="title"><h4> - {book.title}</h4></div>
         <div className ="author">{book.author}</div>
     </li>)
