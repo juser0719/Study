68fa2a88085b922d9826cf00fce4b9b6e0701e8c
juser0719 <sinsa0719@naver.com>
2021-06-03 23:13:29 +0900

    use localStorage

--- FE/ContextAPI/context_tutorial/src/contexts/BookContext.js
+++ FE/ContextAPI/context_tutorial/src/contexts/BookContext.js
@@ -1,30 +1,25 @@
-import React, {createContext, useState , useReducer} from 'react'
-import uuid from 'uuid/v1'
+import React, {createContext, useEffect , useReducer} from 'react'
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 function BookContextProvider(props) {
-    const [books, dispatch] = useReducer(bookReducer,[]);
+    const [books, dispatch] = useReducer(bookReducer,[], ()=>{
+        const localData = localStorage.getItem('books'); 
+        return localData ? JSON.parse(localData) : [] // 있으면 가져옴
+    });
 
-    // const [books, setBooks] = useState([
-    //     {title: 'name of the wind', author:'patrick rothfuss',id: 1},
-    //     {title: 'the way of kings', author:'brandon sanderson', id: 2},
-    // ])
-
-    // const addBook = (title,author) =>{
-    //     setBooks([...books,{title,author, id: uuid() }])
-    // }
-
-    // const removeBook = (id) =>{
-    //     setBooks(books.filter(book => book.id !== id))
-    // }
+    useEffect(()=>{
+        localStorage.setItem('books',JSON.stringify(books))
+    },[books])
+    //추가하거나 삭제하면 작동
 
     return(
         <BookContext.Provider value ={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
+/* */
 }
 
 export default BookContextProvider
