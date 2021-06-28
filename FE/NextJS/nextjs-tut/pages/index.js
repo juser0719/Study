8ec0f790bb92a4817b6bc1aa209d46812fc6f340
juser0719 <sinsa0719@naver.com>
2021-06-28 17:43:56 +0900

    #2

--- FE/NextJS/nextjs-tut/pages/index.js
+++ FE/NextJS/nextjs-tut/pages/index.js
@@ -2,7 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+function getDate (){
+  
+}
+
 export default function Home() {
+  const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+
+  
   return (
     <div>
       create-next-app
