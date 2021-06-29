813feb2bf0010062660ea1f6e05af90a69a170dc
juser0719 <sinsa0719@naver.com>
2021-06-29 20:11:21 +0900

    static

--- FE/NextJS/nextjs-tut/pages/index.js
+++ FE/NextJS/nextjs-tut/pages/index.js
@@ -1,19 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-function getDate (){
-  
-}
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import ItemList from '../src/component/ItemList'
+import { Divider, Header } from 'semantic-ui-react'
 
 export default function Home() {
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-
-  
+  const [list,setList] = useState([]);
+  useEffect(() => {
+    axios.get(API_URL)
+    .then((res) =>{
+    setList(res.data)
+    console.log(res.data)
+    })
+  }, [])
   return (
     <div>
-      create-next-app
-
+      <Head>
+        <title>HOME | JUSER</title>
+      </Head>
+      <Header as ="h3" style={{paddingTop : 40}}>Best Item</Header>
+      <Divider />
+      <ItemList list = {list.slice(0,9)}/>
+      <Header as ="h3" style={{paddingTop : 40}}>New Item</Header>
+      <Divider />
+      <ItemList list = {list.slice(9)}/>
     </div>
   )
 }
--- FE/NextJS/nextjs-tut/src/component/Top.js
+++ FE/NextJS/nextjs-tut/src/component/Top.js
@@ -7,7 +7,7 @@ export default function Top() {
       <div style={{ display: "flex", paddingTop: 20 }}>
         <div style={{ flex: "100px 0 0" }}>
           <img
-            src="/images/angma.png"
+            src="https://cdn.loud.kr/prod/public/thumb/order_sub_1993492_1_170621201736.jpg"
             alt="logo"
             style={{ display: "block", width: 80 }}
           />
