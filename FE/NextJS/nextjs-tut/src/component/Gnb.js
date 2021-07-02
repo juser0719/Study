1638ae6aa3bce1867c8f0aae3aa9c83fb92f7370
juser0719 <sinsa0719@naver.com>
2021-07-02 18:19:39 +0900

    dynamicRouting

--- FE/NextJS/nextjs-tut/src/component/Gnb.js
+++ FE/NextJS/nextjs-tut/src/component/Gnb.js
@@ -6,17 +6,14 @@ export default function Gnb(){
         <Menu.Item
           name='home'
           active={activeItem === 'home'}
-          // onClick={this.handleItemClick}
         />
         <Menu.Item
           name='messages'
           active={activeItem === 'messages'}
-          // onClick={this.handleItemClick}
         />
         <Menu.Item
           name='friends'
           active={activeItem === 'friends'}
-          // onClick={this.handleItemClick}
         />
       </Menu>)
 }
