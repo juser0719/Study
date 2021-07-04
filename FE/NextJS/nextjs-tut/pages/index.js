c1d3f35c387d9616553ee3d34e95314ea44d1ad5
juser0719 <sinsa0719@naver.com>
2021-07-04 22:23:15 +0900

    loading

--- FE/NextJS/nextjs-tut/pages/index.js
+++ FE/NextJS/nextjs-tut/pages/index.js
@@ -4,16 +4,18 @@ import styles from '../styles/Home.module.css'
 import axios from 'axios'
 import {useEffect, useState} from 'react'
 import ItemList from '../src/component/ItemList'
-import { Divider, Header } from 'semantic-ui-react'
+import { Divider, Header, Loader } from 'semantic-ui-react'
 
 export default function Home() {
   const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
   const [list,setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     axios.get(API_URL)
     .then((res) =>{
     setList(res.data)
     console.log(res.data)
+    setIsLoading(false);
     })
   }, [])
   return (
@@ -21,12 +23,26 @@ export default function Home() {
       <Head>
         <title>HOME | JUSER</title>
       </Head>
-      <Header as ="h3" style={{paddingTop : 40}}>Best Item</Header>
-      <Divider />
-      <ItemList list = {list.slice(0,9)}/>
-      <Header as ="h3" style={{paddingTop : 40}}>New Item</Header>
-      <Divider />
-      <ItemList list = {list.slice(9)}/>
+      {
+        isLoading && (<div style= {{padding : "300px 0"}}> 
+          <Loader inline="centered" active>
+            Loading
+          </Loader>
+        </div>)
+      }
+      {
+        !isLoading &&(
+          <>
+          <Header as ="h3" style={{paddingTop : 40}}>Best Item</Header>
+          <Divider />
+          <ItemList list = {list.slice(0,9)}/>
+          <Header as ="h3" style={{paddingTop : 40}}>New Item</Header>
+          <Divider />
+          <ItemList list = {list.slice(9)}/>
+      </>
+        )
+      }
+      
     </div>
   )
 }
