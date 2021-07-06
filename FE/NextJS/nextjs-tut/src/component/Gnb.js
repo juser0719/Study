3496d3f6024f3a43407eceab48b26fa3dc85ab09
juser0719 <sinsa0719@naver.com>
2021-07-06 17:29:57 +0900

    ssr

--- FE/NextJS/nextjs-tut/src/component/Gnb.js
+++ FE/NextJS/nextjs-tut/src/component/Gnb.js
@@ -1,19 +1,32 @@
+import { useRouter } from 'next/router';
 import { Menu } from 'semantic-ui-react';
+
 export default function Gnb(){
-    const activeItem =  "home"
+    let activeItem;
+    const router = useRouter()
+    const goLink = (e,data) =>{
+      if (data.name === 'home'){
+        router.push('/')
+      }else if (data.name === 'about'){
+        router.push('/about')
+      }
+    }
+
+    if (router.pathname === '/')
+      activeItem = "home"
+    else if (router.pathname === '/about')
+      activeItem = "about"
 
     return ( <Menu inverted>
         <Menu.Item
           name='home'
           active={activeItem === 'home'}
+          onClick = {goLink}
         />
         <Menu.Item
-          name='messages'
-          active={activeItem === 'messages'}
-        />
-        <Menu.Item
-          name='friends'
-          active={activeItem === 'friends'}
+          name='about'
+          active={activeItem === 'about'}
+          onClick = {goLink}
         />
       </Menu>)
 }
