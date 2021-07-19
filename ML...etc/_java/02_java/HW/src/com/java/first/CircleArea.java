6f69ea74d3f1ed1d69a145bedc87f6a1344251c3
juser0719 <sinsa0719@naver.com>
2021-07-20 08:50:07 +0900

    기본

---
+++ ML...etc/_java/02_java/HW/src/com/java/first/CircleArea.java
@@ -0,0 +1,7 @@
+package com.java.first;
+
+public class CircleArea {
+public static void main(String[] args) {
+	System.out.println("반지름 5cm 인 원의 넓이는 "+ (5*5*3.14)+ "Cm2입니다. " );
+}
+}
---
+++ ML...etc/_java/02_java/day1/src/com/ssafy/w0714/io/Main.java
@@ -0,0 +1,14 @@
+package com.ssafy.w0714.io;
+
+import java.util.Scanner;
+
+//A+B(BJ 1000)
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		
+		System.out.println(x+y);
+	}
+}
