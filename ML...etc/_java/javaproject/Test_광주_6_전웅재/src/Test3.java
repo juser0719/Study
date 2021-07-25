4ed94f3f668416fa56acd7ebe19d3ca7cf6d71e6
juser0719 <sinsa0719@naver.com>
2021-07-25 19:35:47 +0900

    del

---
+++ ML...etc/_java/javaproject/Test_광주_6_전웅재/src/Test3.java
@@ -0,0 +1,22 @@
+
+public class Test3 {
+
+
+	public void execute(String msg) {
+             // 구현 하세요.
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		Test3 c=new Test3();
+		c.execute("5329053995535987827332679340558347453272569584"); 
+		c.execute("122333444455555666666"); 
+		System.out.println("test3");	
+	}
+}
