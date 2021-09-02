1a354b9ef8e1ce96310bf93d70e7f682a6728c14
juser0719 <sinsa0719@naver.com>
2021-09-02 23:28:04 +0900

    simple_Log_in

--- FE/NextJS/nextjs-tut/pages/admin.js
+++ FE/NextJS/nextjs-tut/pages/admin.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router'
+import { Button } from 'semantic-ui-react';
 export default function Admin() {
-    return <>admin</>
+    const router = useRouter();
+    const [isLogin, setIsLogin] = useState(false);
+    useEffect(() => {
+        axios.get('/api/isLogin')
+            .then(res => {
+                if (res.status === 200 && res.data.name) {
+                    //login
+                    setIsLogin(true);
+                } else {
+                    //not login
+                    router.push("/login");
+                }
+            })
+    }, [])
+
+    const logout = () => {
+        axios.get('/api/logout')
+            .then(res => {
+                if (res.status === 200) {
+                    router.push("/")
+                }
+            })
+    }
+    return <>admin{isLogin && <Button onClick={logout}>LogOut</Button>}</>
 }
