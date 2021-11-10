bde234879115867d9648cec5435ef87a10c5ac61
전웅재 <sinsa0719@naver.com>
2021-11-10 11:42:52 +0900

    ws4

---
+++ BE/Spring/Vue_Book_API/src/main/java/com/ssafy/book/model/service/BookService.java
@@ -0,0 +1,14 @@
+package com.ssafy.book.model.service;
+
+import java.util.List;
+
+import com.ssafy.book.model.BookDto;
+
+public interface BookService {
+	List<BookDto> list();
+	BookDto search(String isbn);
+	boolean create(BookDto bookDto);
+	boolean modify(BookDto bookDto);
+	boolean delete(String isbn);
+
+}
