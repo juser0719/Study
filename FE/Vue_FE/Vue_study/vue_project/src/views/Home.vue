532b4ea2eb94432cdee712a422e7c412b551f480
전웅재 <sinsa0719@naver.com>
2021-11-11 11:30:12 +0900

    vue_cli

---
+++ FE/Vue_FE/Vue_study/vue_project/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld :msg="sendMsg" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      sendMsg: "킹갓웅짱짱",
+    };
+  },
+};
+</script>
